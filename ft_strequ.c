d5b98a40e37980e126224ffbd8a6942f17c16c93
César Court <44904159+CesarCourt@users.noreply.github.com>
2018-11-23 15:29:41 +0100

    Add files via upload
    Correction de 2 fonctions

--- ft_strequ.c
+++ ft_strequ.c
@@ -17,7 +17,7 @@ int			ft_strequ(char const *s1, char const *s2)
 	i = 0;
 	if (!(s1 && s2))
 		return (0);
-	while (s1[i] && s2[i])
+	while (s1[i] || s2[i])
 	{
 		if (s1[i] != s2[i])
 			return (0);
--- ft_strnequ.c
+++ ft_strnequ.c
@@ -19,7 +19,7 @@ int			ft_strnequ(char const *s1, char const *s2, size_t n)
 	i = 0;
 	if (!(s1 && s2))
 		return (0);
-	while ((s1[i] && s2[i]) && i < n)
+	while ((s1[i] || s2[i]) && i < n)
 	{
 		if (s1[i] != s2[i])
 			return (0);
