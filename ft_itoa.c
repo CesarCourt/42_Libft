7f7cccbf93224f33fdd927db06a9a582705e2f94
César Court <44904159+CesarCourt@users.noreply.github.com>
2018-11-27 19:12:37 +0100

    Add files via upload
    Ajout ft_itoa.c

--- ft_itoa.c
+++ ft_itoa.c
@@ -10,4 +10,36 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+static int		ft_count_nbr(int nb)
+{
+	unsigned	nbr;
+	int			total;
+
+	if (nb < 0)
+	{
+		total++;
+		nbr = nb * -1;
+	}
+	else
+		nbr = nb;
+	if (nbr > 9)
+	{
+		total++;
+		ft_putnbr(nbr / 10);
+	}
+	else
+		total++;
+	return (total);
+}
+
+
+char		*ft_itoa(int n)
+{
+	char *dst;
+
+	if (!(dst = (char*)ft_memalloc(sizeof(char) * ft_count_nbr(n) + 1)))
+		return (NULL);
+
+}
